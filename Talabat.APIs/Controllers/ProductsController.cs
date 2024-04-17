6285e985c1da24a3f42349cf8a3b8a495f68975f
MahmoudKhalefa2020 <mahmoud.khalefa757@gmail.com>
2024-04-17 13:09:11 +0200

    Part 02 Refactor GetProducts Using Specification DP

--- Talabat.APIs/Controllers/ProductsController.cs
+++ Talabat.APIs/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Talabat.Core.Entities;
 using Talabat.Core.Repositories;
+using Talabat.Core.Specifications.ProductSpecs;
 
 namespace Talabat.APIs.Controllers
 {
@@ -16,7 +17,8 @@ namespace Talabat.APIs.Controllers
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
         {
-            var products = await _ProductRepo.GetAllAsync();
+            var spec = new ProductWithBrandAndCategorySpecifications();
+            var products = await _ProductRepo.GetWithSpecAsync(spec);
             return Ok(products);
         }
 
---
+++ Talabat.Core/Specifications/ProductSpecs/ProductWithBrandAndCategorySpecifications.cs
@@ -0,0 +1,15 @@
+﻿using Talabat.Core.Entities;
+
+namespace Talabat.Core.Specifications.ProductSpecs
+{
+    public class ProductWithBrandAndCategorySpecifications : BaseSpecifications<Product>
+    {
+        public ProductWithBrandAndCategorySpecifications()
+            : base()
+        {
+            Includes.Add(P => P.Brand);
+            Includes.Add(P => P.Category);
+
+        }
+    }
+}
