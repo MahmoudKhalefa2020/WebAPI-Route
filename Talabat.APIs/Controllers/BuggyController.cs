ddc2ed277333043fb083bf9e127c0a64ab25ce60
MahmoudKhalefa2020 <mahmoud.khalefa757@gmail.com>
2024-04-17 14:57:07 +0200

    Part 09 NotFound, BadRequest and Unauthorized Error Handling

---
+++ Talabat.APIs/Controllers/BuggyController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Talabat.Repository.Data;
+
+namespace Talabat.APIs.Controllers
+{
+
+    public class BuggyController : BaseApiController
+    {
+        private readonly StoreContext _dbContext;
+
+        public BuggyController(StoreContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundRequest()
+        {
+            var Product = _dbContext.Products.Find(100);
+            if (Product is null) return NotFound();
+            return Ok(Product);
+
+
+        }
+
+
+        [HttpGet("servererror")]
+        public ActionResult GetServerError()
+        {
+            var product = _dbContext.Products.Find(100);
+            var productToReturn = product.ToString();
+
+            return Ok(productToReturn);
+        }
+
+
+        [HttpGet("badrequest")]
+        public ActionResult GetBadRequest()
+        {
+            return BadRequest();
+        }
+
+        [HttpGet("badrequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok();
+        }
+    }
+}
